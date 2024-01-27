93b949262714c15a834e5fababa444c1f625dc20
Zikrulloh0405 <zikrillohakramov2@gmail.com>
2024-01-27 08:58:20 +0500

    1

--- test/programming_assignment_3_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:programming_assignment_3/programming_assignment_3.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
